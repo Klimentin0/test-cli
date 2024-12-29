a37912297b709f4098ea2dc52259950ea6768562
Klimentin0 <meshkov.klim@gmail.com>
2024-12-29 21:31:19 +0300

    создание тестовой repl-програмки

--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Test prompt>>>")
+		reader.Scan()
+
+		strSlice := cleanInput(reader.Text())
+		if len(strSlice) == 0 {
+			continue
+		}
+
+		commandName := strSlice[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	strSlice := strings.Fields(text)
